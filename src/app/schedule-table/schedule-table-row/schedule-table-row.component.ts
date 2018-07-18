e8cceeb12c01036598f7d0e80e338bea7fb8fd89
elenakyrychenko93 <elena.kyrychenko93@gmail.com>
2018-07-18 17:57:31 +0300

    broken voice

--- src/app/schedule-table/schedule-table-row/schedule-table-row.component.ts
+++ src/app/schedule-table/schedule-table-row/schedule-table-row.component.ts
@@ -13,51 +13,12 @@ export class ScheduleTableRowComponent implements OnInit {
   heroes = HEROES;
   oldAction;
   newAction;
-  date;
-  time;
-  speechTime;
-  utterance;
-  intervalHandler;
-
   @Output() onDelete = new EventEmitter<string>();
   @Output() onEdit = new EventEmitter<any>();
 
-  constructor() {
-    this.intervalHandler = setInterval(self => self.getTime(), 5000, this);
-  }
+  constructor() {  }
 
-  ngOnInit() {
-
-  }
-
-  getTime() {
-    this.date = new Date();
-    this.time = this.addZero(this.date.getHours())+':'+this.addZero(this.date.getMinutes());
-    // this.time = this.date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
-    console.log(this.time);
-    this.checkTime(HEROES);
-  }
-
-  addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-
-  checkTime(obj) {
-    for(let item of obj) {
-      // console.log(item.startTime);
-      if (item.startTime == this.time) {
-        console.log(item.startTime, item.description);
-        this.utterance = new SpeechSynthesisUtterance(item.description);
-        this.utterance.lang = 'ru-RU';
-        this.utterance.rate = 0.7;
-        this.utterance.pitch = 1.2;
-        window.speechSynthesis.speak(this.utterance);
-      };
-    }
-  }
+  ngOnInit() {  }
 
   deleteItem(id) {
     this.onDelete.emit(id);
--- src/app/schedule-table/schedule-table.component.ts
+++ src/app/schedule-table/schedule-table.component.ts
@@ -1,27 +1,73 @@
 import {Component, Input, OnInit, Output} from '@angular/core';
-import {Hero} from '../data';
+import {Hero, HEROES} from '../data';
 
 @Component({
   selector: 'app-schedule-table',
   templateUrl: './schedule-table.component.html',
   styleUrls: ['./schedule-table.component.css']
 })
-export class ScheduleTableComponent {
-  // editModeValue = false;
+export class ScheduleTableComponent implements OnInit {
   heroes = Hero;
-  constructor() { }
+  date;
+  time;
+  utterance;
+  intervalHandler;
+  audio;
+
+  constructor() { this.intervalHandler = setInterval(self => self.getTime(), 25000, this); }
+
+  ngOnInit() {
+
+  }
+  getTime() {
+    this.date = new Date();
+    this.time = this.addZero(this.date.getHours()) + ':' + this.addZero(this.date.getMinutes());
+    // this.time = this.date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    this.checkTime(HEROES);
+  }
+
+  addZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  checkTime(obj) {
+    for (let item of obj) {
+        if (item.startTime == this.time && obj[0].startTime == this.time) {
+          this.playAudio();
+          this.readDescription(item);
+        } else if (item.startTime == this.time) {
+          this.readDescription(item);
+        };
+    }
+  }
+
+  readDescription(item) {
+    this.utterance = new SpeechSynthesisUtterance(item.description);
+    this.utterance.lang = 'en-EN';
+    this.utterance.rate = 0.7;
+    this.utterance.pitch = 1.2;
+    window.speechSynthesis.speak(this.utterance);
+    console.log(this.utterance);
+  }
+
+  playAudio(){
+    this.audio = new Audio();
+    console.log(this.audio.src);
+    this.audio.src = '../../assets/sounds/alarm.mp3';
+    this.audio.volume = 0.1;
+    this.audio.load();
+    this.audio.play();
+  }
 
   onDelete(id) {
     this.heroes.deleteAction(id);
   }
   onEdit(data) {
-    // console.log(data.inputsData);
     this.heroes.updateAction(data.id, data.inputsData);
   }
-  // onAdd(data) {
-  //   console.log(data);
-  //   this.heroes.addAction(data);
-  // }
 
 }
 
